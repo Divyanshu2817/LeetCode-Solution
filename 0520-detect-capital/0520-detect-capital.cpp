f89ee98e0781d63caafa83b0c2699d3f01672647
Divyanshu <devdivyanshuvarshney@gmail.com>
2026-06-24 12:02:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.2%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int capitalCount = 0;
+        for (char ch : word) {
+            if (ch >= 'A' && ch <= 'Z') {
+                capitalCount++;
+            }
+        }
+        if (capitalCount == word.length()) {
+            return true;
+        }
+        
+        if (capitalCount == 0) {
+            return true;
+        }
+        
+        if (capitalCount == 1 && (word[0] >= 'A' && word[0] <= 'Z')) {
+            return true;
+        }
+        return false;
+    }
+};
