10b69228a77f98a882b6f567b852bc6df72c8dbf
Divyanshu <devdivyanshuvarshney@gmail.com>
2026-06-26 10:29:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.51%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,12 +4,9 @@ public:
         if(n<=0){
             return false;
         }
-        if(n==1){
-            return true;
+        while(n%3==0){
+            n=n/3;
         }
-        if (n%3!=0){
-            return false;                                         
-        }
-        return isPowerOfThree(n/3); 
+        return n==1; 
     }
 };
