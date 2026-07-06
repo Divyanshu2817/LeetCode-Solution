059685d899200c13a9072d75bf9e597345982a33
Divyanshu <devdivyanshuvarshney@gmail.com>
2026-07-06 11:38:27 +0530

    Time: 37 ms (78.31%), Space: 71 MB (84.56%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    void merge(vector<int>& nums,int l,int mid,int r){
-        int n1=mid-l+1;
-        int n2=r-mid;
-        vector<int> left(n1),right(n2);
-        for(int i=0;i<n1;i++){
-            left[i]=nums[l+i];
+    int partition(vector<int>& nums, int l, int r) {
+        int pivot = nums[l + rand() % (r - l + 1)];
+        int i = l, j = r;
+        while (i <= j) {
+            while (nums[i] < pivot) i++;
+            while (nums[j] > pivot) j--;
+            if (i <= j) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
         }
-        for(int i=0;i<n2;i++){
-            right[i]=nums[mid+1+i];
-        }
-        int i = 0, j = 0, k = l;
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) nums[k++] = left[i++];
-            else nums[k++] = right[j++];
-        }
-        while (i < n1) nums[k++] = left[i++];
-        while (j < n2) nums[k++] = right[j++];
+        return i;
     }
     
-    void mergeSort(vector<int>& nums, int l, int r) {
+    void quickSort(vector<int>& nums, int l, int r) {
         if (l >= r) return;
-        int mid = l + (r - l) / 2;
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid + 1, r);
-        merge(nums, l, mid, r);
+        int idx = partition(nums, l, r);
+        quickSort(nums, l, idx - 1);
+        quickSort(nums, idx, r);
     }
-
+    
     vector<int> sortArray(vector<int>& nums) {
-        int n=nums.size();
-        if(n<=1) return nums;
-        mergeSort(nums,0,n-1);
+        int n = nums.size();
+        if (n <= 1) return nums;
+        quickSort(nums, 0, n - 1);
         return nums;
     }
 };
