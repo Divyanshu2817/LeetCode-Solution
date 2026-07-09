83fd03f6c5645b10ba289eb7c0e5c33e5b4222cb
Divyanshu <devdivyanshuvarshney@gmail.com>
2026-07-09 12:59:09 +0530

    Time: 1 ms (100%), Space: 43.7 MB (14.24%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,37 +1,29 @@
-import java.util.Stack;
-
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<Integer> st = new Stack<>();
-        
-        for (int i = 0; i < operations.length; i++) {
-            if (operations[i].equals("C")) {
-                if (!st.isEmpty()) {
-                    st.pop();
-                }
-            } 
-            else if (operations[i].equals("D")) {
-                if (!st.isEmpty()) {
-                    st.push(st.peek() * 2);
-                }
-            } 
-            else if (operations[i].equals("+")) {
-                int top1 = st.pop();
-                int top2 = st.peek();
-                
-                st.push(top1);
-                st.push(top1 + top2);
-            } 
-            else {
-                st.push(Integer.parseInt(operations[i]));
+        int l=operations.length;
+        int[] arr = new int[l];
+        int j=0;
+        int s=0;
+        for(int i=0;i<l;i++){
+            if(operations[i].equals("C")){
+                j--;
+            }
+            else if(operations[i].equals("D")){
+                arr[j]=(2*arr[j-1]);
+                j++;
+            }
+            else if(operations[i].equals("+")){
+                arr[j]=(arr[j-2]+arr[j-1]);
+                j++;
+            }
+            else{
+                arr[j]=Integer.parseInt(operations[i]);
+                j++;
             }
         }
-        
-        int totalSum = 0;
-        while (!st.isEmpty()) {
-            totalSum += st.pop();
+        for(int k=0;k<j;k++){
+            s=s+arr[k];
         }
-        
-        return totalSum;
+        return s;
     }
 }
